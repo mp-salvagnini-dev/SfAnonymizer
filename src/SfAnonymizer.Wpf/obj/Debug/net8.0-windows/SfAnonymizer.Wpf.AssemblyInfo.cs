a445e462be4b356e3df964bf4efa60c2af4cc254
Matteo <matteo.polonio@salvagninigroup.com>
2026-03-17 13:31:57 +0100

    Changes

--- src/SfAnonymizer.Wpf/obj/Debug/net8.0-windows/SfAnonymizer.Wpf.AssemblyInfo.cs
+++ src/SfAnonymizer.Wpf/obj/Debug/net8.0-windows/SfAnonymizer.Wpf.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITSMTicketAnonymizer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f587f60f6890c7004d8118bebdd6a7277268f2ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITSMTicketAnonymizer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITSMTicketAnonymizer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
