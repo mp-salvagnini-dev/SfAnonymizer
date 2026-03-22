36828b161696a39f176206c223dd467730529d8a
Matteo <matteo.polonio@salvagninigroup.com>
2026-03-22 10:47:26 +0100

    Changes?

--- src/SfAnonymizer.Core/obj/Debug/net8.0/SfAnonymizer.Core.AssemblyInfo.cs
+++ src/SfAnonymizer.Core/obj/Debug/net8.0/SfAnonymizer.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SfAnonymizer.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+277ec62bcea7986968ab3734e276b594e82031cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a445e462be4b356e3df964bf4efa60c2af4cc254")]
 [assembly: System.Reflection.AssemblyProductAttribute("SfAnonymizer.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SfAnonymizer.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/SfAnonymizer.Wpf/obj/Debug/net8.0-windows/SfAnonymizer.Wpf.AssemblyInfo.cs
+++ src/SfAnonymizer.Wpf/obj/Debug/net8.0-windows/SfAnonymizer.Wpf.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITSMTicketAnonymizer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+277ec62bcea7986968ab3734e276b594e82031cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a445e462be4b356e3df964bf4efa60c2af4cc254")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITSMTicketAnonymizer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITSMTicketAnonymizer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
